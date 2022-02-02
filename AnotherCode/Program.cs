e5939f2d8bfeba9dd71da28527d119e6b4182a1d
bibabbly <bizimungu2004@gmail.com>
2022-02-02 15:56:23 +0200

    OOP in C #

---
+++ AnotherCode/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+try {
+    Console.Write("Enter First Number : ");
+    int num1 = Convert.ToInt32(Console.ReadLine());
+
+    Console.Write("Enter Second Number : ");
+    int num2 = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("The answer of division is " + (num1 / num2));
+
+}
+catch(Exception e) {
+    Console.WriteLine("You have Error in Division " +e.Message);
+}
